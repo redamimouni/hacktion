3635c6fc69eaa53ea6735bfcc3ec772f7addfc90
Mégane Menanteau <mmenanteau@octo.com>
2019-03-30 14:00:08 +0100

    Add countdown and carousel to homepage

--- Podfile
+++ Podfile
@@ -1,11 +1,12 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-target 'hacktion' do
+target 'Pilyumi' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
+
   pod 'Firebase/Core'
   pod 'Firebase/Messaging'
-  # Pods for hacktion
+  pod 'iCarousel', '1.8'
 
 end
--- hacktion/ViewController.swift
+++ hacktion/ViewController.swift
@@ -7,14 +7,64 @@
 //
 
 import UIKit
+import iCarousel
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var countdownLabel: UILabel!
+  @IBOutlet weak var carouselView: iCarousel!
+  
+  var timer: Timer!
+  var nextPillDate: Date!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    nextPillDate = Date(timeIntervalSinceNow: 5000)
+    
+    carouselView.reloadData()
+    carouselView.type = .linear
+    carouselView.currentItemIndex = carouselView.numberOfItems / 2
   }
-
-
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateCountdown), userInfo: nil, repeats: true)
+  }
+  
+  @objc private func updateCountdown() {
+    let currentDate = Date()
+    
+    guard nextPillDate > currentDate else {
+      timer.invalidate()
+      return
+    }
+    let dateComponents = Calendar.current.dateComponents([.hour, .minute, .second], from: currentDate, to: nextPillDate)
+    countdownLabel.text = "\(dateComponents.hour!):\(dateComponents.minute!):\(dateComponents.second!)"
+  }
 }
 
+extension ViewController: iCarouselDataSource {
+  func numberOfItems(in carousel: iCarousel!) -> Int {
+    return 5
+  }
+  
+  func carousel(_ carousel: iCarousel!, viewForItemAt index: Int, reusing view: UIView!) -> UIView! {
+    let view = UIView(frame: CGRect(x: 0, y: 0, width: carousel.frame.width - 160, height: carousel.frame.height))
+    view.backgroundColor = index % 2 == 0 ? .red : .blue
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: carousel.frame.width, height: 50))
+    label.text = "Titre"
+    view.addSubview(label)
+    return view
+  }
+}
+
+extension ViewController: iCarouselDelegate {
+  func carousel(_ carousel: iCarousel!, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+    if option == .spacing {
+      return value * 1.05
+    }
+    return value
+  }
+}
