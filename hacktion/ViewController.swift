1ed8906c17c7a342a78cd0d547e9044012ae5cd3
Mégane Menanteau <mmenanteau@octo.com>
2019-03-30 18:58:18 +0100

    Set calendar cells colors

--- hacktion/ViewController.swift
+++ hacktion/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
   
   private var timer: Timer!
   private var nextPillDate: Date!
+  
   private let validatedPillDates: [Date] = [
     dateFormatter.date(from: "2019-03-01")!,
     dateFormatter.date(from: "2019-03-02")!,
@@ -34,13 +35,21 @@ class ViewController: UIViewController {
     dateFormatter.date(from: "2019-03-07")!,
     dateFormatter.date(from: "2019-03-08")!,
     dateFormatter.date(from: "2019-03-09")!,
-    dateFormatter.date(from: "2019-03-10")!
+    dateFormatter.date(from: "2019-03-15")!,
+    dateFormatter.date(from: "2019-03-16")!
+  ]
+  private let menstruationDates: [Date] = [
+    dateFormatter.date(from: "2019-03-10")!,
+    dateFormatter.date(from: "2019-03-11")!,
+    dateFormatter.date(from: "2019-03-12")!,
+    dateFormatter.date(from: "2019-03-13")!,
+    dateFormatter.date(from: "2019-03-14")!
   ]
   
   private let colors: [UIColor] = [
     UIColor(red: 108/255, green: 184/255, blue: 255/255, alpha: 1),
     UIColor(red: 1/255, green: 140/255, blue: 203/255, alpha: 1),
-     UIColor(red: 105/255, green: 55/255, blue: 146/255, alpha: 1),
+    UIColor(red: 105/255, green: 55/255, blue: 146/255, alpha: 1),
     UIColor(red: 0/255, green: 86/255, blue: 180/255, alpha: 1)
   ]
   
@@ -49,9 +58,7 @@ class ViewController: UIViewController {
     
     nextPillDate = Date(timeIntervalSinceNow: 5000)
     
-    validatedPillDates.forEach { date in
-      calendarView.select(date)
-    }
+    calendarView.clipsToBounds = true
     
     carouselView.reloadData()
     carouselView.type = .linear
@@ -82,35 +89,37 @@ extension ViewController: iCarouselDataSource {
   }
   
   func carousel(_ carousel: iCarousel!, viewForItemAt index: Int, reusing view: UIView!) -> UIView! {
-    let view = UIView(frame: CGRect(x: 0, y: 0, width: 244, height: 239))
+    let marginX: CGFloat = 65
+    
+    let itemView = UIView(frame: CGRect(x: 0, y: 0, width: carousel.frame.width - marginX * 2, height: carousel.frame.height))
     if index == 2 {
         let checkinView = Bundle.main.loadNibNamed("CheckinView", owner: self, options: nil)?.first as? CheckinView
         checkinView?.initViews()
-        view.addSubview(checkinView!)
+        itemView.addSubview(checkinView!)
     } else if index == 1 {
         let moodView = Bundle.main.loadNibNamed("MoodMeter", owner: self, options: nil)?.first as? MoodMeter
         moodView?.initViews()
-        view.addSubview(moodView!)
+        itemView.addSubview(moodView!)
     } else if index == 0 {
         let streakView = Bundle.main.loadNibNamed("Streak", owner: self, options: nil)?.first as? Streak
-        view.addSubview(streakView!)
+        itemView.addSubview(streakView!)
     } else if index == 3 {
         let funfactsView = Bundle.main.loadNibNamed("Funfacts", owner: self, options: nil)?.first as? Funfacts
-        view.addSubview(funfactsView!)
+        itemView.addSubview(funfactsView!)
     }
-    view.layer.cornerRadius = 5
-    view.layer.shadowColor = UIColor.black.cgColor
-    view.layer.shadowOpacity = 10
-    view.layer.shadowOffset = CGSize.zero
-    view.layer.shadowRadius = 1
-    return view
+    itemView.layer.cornerRadius = 5
+    itemView.layer.shadowColor = UIColor.black.cgColor
+    itemView.layer.shadowOpacity = 10
+    itemView.layer.shadowOffset = CGSize.zero
+    itemView.layer.shadowRadius = 1
+    return itemView
   }
 }
 
 extension ViewController: iCarouselDelegate {
   func carousel(_ carousel: iCarousel!, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
     if option == .spacing {
-      return value * 1.05
+      return value * 1.07
     }
     return value
   }
@@ -121,7 +130,17 @@ extension ViewController: iCarouselDelegate {
 }
 
 extension ViewController: FSCalendarDataSource {
+  func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+    return Calendar.current.isDate(date, inSameDayAs: Date()) ? 1 : 0
+  }
 }
 
 extension ViewController: FSCalendarDelegate {
+  func calendar(_ calendar: FSCalendar, willDisplay cell: FSCalendarCell, for date: Date, at monthPosition: FSCalendarMonthPosition) {
+    if (validatedPillDates.contains(date)) {
+      cell.shapeLayer.fillColor = UIColor(red: 86/255, green: 205/255, blue: 113/255, alpha: 1).cgColor
+    } else if (menstruationDates.contains(date)) {
+      cell.shapeLayer.fillColor = UIColor.red.cgColor
+    }
+  }
 }
